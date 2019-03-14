9faa4f38623e55db54b3c0df0dbe350d80a863a3
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-03-14 06:32:55 -0700

    Add files via upload

---
+++ Classroom_Lecture_Model_Series_Journal_Article_Management_System.R
@@ -0,0 +1,65 @@
+#----------------------------------------Journal Article Management System----------------------------------
+
+Class.Lecture.Math.Organization<-c("Introduction",
+                                  "List of Articles",
+                                  "Main Article",
+                                  "Plan of Lecture",
+                                  "Background: Chemistry",
+                                  "Background: Biology",
+                                  "Background: Gene Dictionary",
+                                  "Background: Biology Diagram",
+                                  "Background: Biology Network",
+                                  "Background: Mathematics",
+                                  "Background: Definitions",
+                                  "Background: Theorems",
+                                  "Background: Equations",
+                                  "Numerical Algorithms",
+                                  "Results",
+                                  "Results: Tables",
+                                  "Results: Figures",
+                                  "Classroom Discussion",
+                                  "Classroom Assignments",
+                                  "References")
+
+
+Class.Lecture.Music.Organization<-c("Introduction",
+                                    "Main Composition",
+                                    "Plan of Lecture",
+                                    "Background: Music Theory",
+                                    "Background: Music Motifs",
+                                    "Background: Composition Diagram",
+                                    "Background: Instrumentation",
+                                    "Background: Motif Variation By Genre",
+                                    "Background: Mathematics",
+                                    "Background: Definitions",
+                                    "Background: Theorems",
+                                    "Background: Equations",
+                                    "Numerical Algorithms",
+                                    "Results",
+                                    "Results: Tables",
+                                    "Results: Figures",
+                                    "Classroom Discussion",
+                                    "Classroom Assignments",
+                                    "References")
+
+
+Diagrams<-list(A=c("Melodic Composite Sequences","Hierachical Markov Model","Amino Acid Model",
+                   "Ribosome Endoplasmic Rectilium and Nucleus Model",
+                   "Signal Transduction Network Model","Plant Phytochemical Model","Metabolic Pathways","Plant Phyllotaxis Gene Regulation",
+                   "Plant Immune System","Pathogen Based Immune System","Chaperone Model","Proteasome Model","Plant Species and Cultivar Model"))
+
+Journal.Name<-c("CL_Cancer Letters","CMA_Computer and Mathematics with Applications", "AM_Discrete Applied Mathematics","EM_Economic Modelling",
+                "AML_Applied Mathematics Letters","SPL_Statistics and Probability Letters","NN_Neural Networks",
+                "CSDA_Computational Statistics and Data Analysis","IJMI_International journal of Medical Informatics")
+
+
+
+
+
+
+
+
+
+
+
+
