23267a5a725829fe63521d0d5ec9a2f48326f495
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-03-20 04:41:33 -0700

    Update Sample_Article_Notes_Design_From_Classroom_Lecture_R_Program.R

--- Sample_Article_Notes_Design_From_Classroom_Lecture_R_Program.R
+++ Sample_Article_Notes_Design_From_Classroom_Lecture_R_Program.R
@@ -6,23 +6,21 @@ library(sampling);library(PearsonDS);library(KEGGgraph);library(keggorthology);l
 library(igraph);library(jsonlite);library(tsDyn);library(tseriesChaos);library(wavelets);library(waveslim);library(readr);library(randtests)
 library(changepoint);library(car);library(tuneR);library(diffEq);library(phaseR)
 #----------------------------------------------------------------------------------------------------------------------------------#
-
 Table.1.df<-data.frame()
 Table.2.df<-data.frame()
 Table.3.df<-data.frame()
 Table.4.df<-data.frame()
 Table.5.df<-data.frame()
 Table.6.df<-data.frame()
-
 Gene.Study<-c("LD","ATRp","p53","p53p","Mdm2c","Mdm2cp","Mdm2n","Aktp","PIP3","PTEN","DDB2","p21tot",
               "p21CE","CycEtot","E2F1","Rbp","Bax","CytoC","Apaf1","Apops","Casp9","Casp3")
 Gene.Study.Literature<-c("ATR","p53","p53","Mdm2","Akt","PIP3","PTEN","DDB2","p21",
                          "CycE","E2F1","Rbp","Bax","CytoC","Apaf1","Apops","Casp9","Casp3")
-#-------------------Noise Processes------------------------
+#-------------------Noise Processes------------------------#
 noise.white<-noise(kind = c("white"))
 noise.pink<-noise(kind = c("pink"))
 noise.red<-noise(kind = c("red"))
-#--------------------Power law-----------------------------
+#--------------------Power law-----------------------------#
 noise.4.4<-noise(kind=c("power"),alpha=(4/4))
 noise.5.4<-noise(kind=c("power"),alpha=(5/4))
 noise.6.4<-noise(kind=c("power"),alpha=(6/4))
@@ -33,9 +31,9 @@ noise.test.parameters <-c(k.noise.1=0,k.noise.2=0,k.noise.3=0,k.noise.4=0,k.nois
                           k.noise.9=0,k.noise.10=0,k.noise.11=0,k.noise.12=0,k.noise.13=0,k.noise.14=0,k.noise.15=0,k.noise.16=0,
                           k.noise.17=0,k.noise.18=0,k.noise.19=0,k.noise.20=0,k.noise.21=0,k.noise.22=0)
 
-#----------------------------------------------------Algorithm Development-------------------------------------------
-Differential.Equation.Algorithms = c("lsoda", "lsode", "lsodes", "lsodar", "vode", "daspk", "euler", "rk4", "ode23", "ode45", "radau", "bdf", "bdf_d", "adams", "impAdams", "impAdams_d", 
-           "iteration")
+#----------------------------------------------------Algorithm Development-------------------------------------------#
+Differential.Equation.Algorithms = c("lsoda", "lsode", "lsodes", "lsodar", "vode", "daspk", "euler", "rk4", 
+                                     "ode23", "ode45", "radau", "bdf", "bdf_d", "adams", "impAdams", "impAdams_d", "iteration")
 
 Algorithm.1<-function(X)
 {
@@ -56,8 +54,23 @@ Algorithm.3<-function(X)
   output<-list()
   return(output)
 }
-#---------------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------Parameter Matrix-----------------------------------------------------#
+
+#------------------------------------------------Equation Systems------------------------------------------------------#
+
+
+#------------------------------------------------DE Solvers, Optimizers and Algorithms--------------------------------#
+
+
+#------------------------------------------------Network--------------------------------------------------------------#
 Network.A<-c("UV","ATR","ATRp","RPA")
 Network.B<-c("p21","p21CE","DDb2","Rb","Rbp","Cyclin E","EF21")
 Network.C<-c("p53","p53p","Pten","Mdm2n","Mdm2c","Mdm2cp","Akt","Aktp","PIP2","PIP3")
 Network.D<-c("BAX","CytoC","Apops","Apaf-1","CASP9","Procasp9","CASP3","Procasp3","PARP1","PARP3")
+
+#------------------------------------------------Tables--------------------------------------------------------------#
+
+
+#------------------------------------------------Figures-------------------------------------------------------------#
+
+#------------------------------------------------References----------------------------------------------------------#
