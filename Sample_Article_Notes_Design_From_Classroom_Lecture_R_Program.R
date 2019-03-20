4fa29a2d650ca995f46f73130e692b46f1fd674a
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-03-20 04:31:54 -0700

    Update Sample_Article_Notes_Design_From_Classroom_Lecture_R_Program.R

--- Sample_Article_Notes_Design_From_Classroom_Lecture_R_Program.R
+++ Sample_Article_Notes_Design_From_Classroom_Lecture_R_Program.R
@@ -1,3 +1,8 @@
 #-----------------------------R Program for InClass Lecture---------------------------------------------------------#
 #-------------------------------------------------------------------------------------------------------------------#
 #-------------------------------------------------------------------------------------------------------------------#
+
+library(pracma);library(xtable);library(deSolve);library(ReacTran);library(rootSolve);library(fda);library(igraph);library(boot)
+library(sampling);library(PearsonDS);library(KEGGgraph);library(keggorthology);library(KEGGREST);library(Rgraphviz)
+library(igraph);library(jsonlite);library(tsDyn);library(tseriesChaos);library(wavelets);library(waveslim);library(readr);library(randtests)
+library(changepoint);library(car);library(tuneR);library(diffEq);library(phaseR)
