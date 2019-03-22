a78d04e778d43fbdac0f1716e3b4aef22aac3d9b
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-03-22 04:53:01 -0700

    Update Reading_List_aRxiv.R

--- Reading_List_aRxiv.R
+++ Reading_List_aRxiv.R
@@ -1,7 +1,8 @@
+#---------------Reading List Design from aRXiv for Classroom Student Presentations-----------------#
 library(aRxiv)
 
-
-
-
-
+#---------------Examples from Lectures------------------------------------#
+ode<-arxiv_search(query = 'ti:ordinary differential equations', limit=100)
+dde<-arxiv_search(query = 'ti:delayed differential equations', limit=100)
+sde<-arxiv_search(query = 'ti:stochastic differential equations', limit=100)
 fde<-arxiv_search(query = 'ti:fractional differential equations', limit=100)
