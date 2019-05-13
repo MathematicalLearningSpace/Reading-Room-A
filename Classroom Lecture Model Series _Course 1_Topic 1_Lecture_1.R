22965da5a22f969b88c767460fedc6700fa5f7e2
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-13 15:17:12 -0700

    Update Classroom Lecture Model Series _Course 1_Topic 1_Lecture_1.R

--- Classroom Lecture Model Series _Course 1_Topic 1_Lecture_1.R
+++ Classroom Lecture Model Series _Course 1_Topic 1_Lecture_1.R
@@ -1,14 +1,38 @@
 #-------------------------------------------------------------------------#
 #--------------------Classroom Lecture Model Series-----------------------#
 #-------------------------------------------------------------------------#
-library(xtable)
-library(GA)
-library(igraph)
-library(pracma}
+library(xtable);library(GA);library(igraph);library(pracma};library(readr);require(BioMedR)
 #----------------------------------R Source Files------------------------------------#
-
 #----------------------------------Data----------------------------------------------#
+#---Example from PubChem for the Classroom-------------------------------------------#
+Gene_Targets_Patents.df <- as.data.frame(read_csv("Gene_Targets_Patents.csv"))
+Protein_Targets_Patents.df <- as.data.frame(read_csv("Protein_Targets_Patents.csv"))
+Enzyme_Targets_Patents.df <- as.data.frame(read_csv("Enzyme_Targets_Patents.csv"))
 
+Review.Article.Module.1<-function(X,search.terms,Z)
+{
+ids<-unique(grep(search.terms,X$patentabstract))
+Article.Content<-""
+Article.Bibliography<-""
+for(i in 1:length(ids))
+{
+  Article.Content<-stringr::str_c(Article.Content,
+                                    X$patentabstract[ids[i]],
+                                    "\\cite{",
+                                     X$pid[ids[i]],"}"," \\\n "
+  )
+  Article.Bibliography<-stringr::str_c(Article.Bibliography,
+                                   X$pid[ids[i]], " & ",
+                                   X$patenttitle[ids[i]]," \\ "
+                                   )
+}
+Article.Review<-stringr::str_c(Article.Content,Article.Bibliography)
+title<-stringr::str_c("Working_Review_Article_",search.terms,"_",Z,"_Target_Patents",".txt")
+base::write(Article.Review,file=title,append=FALSE)
+}
+test.Review.Article.Module.1<-Review.Article.Module.1(Gene_Targets_Patents.df,"cancer","Gene")
+test.Review.Article.Module.2<-Review.Article.Module.1(Protein_Targets_Patents.df,"cancer","Protein")
+test.Review.Article.Module.3<-Review.Article.Module.1(Enzyme_Targets_Patents.df,"cancer","Enzyme")
 
 #----------------------------------Transformations-----------------------------------#
 
