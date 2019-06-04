beeb26f5e575b6f2dbcebd792dee6328a96b9004
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-06-04 07:33:43 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_13_14_15_R_5.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_13_14_15_R_5.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_13_14_15_R_5.R
@@ -5,3 +5,59 @@
 
 %--------------------Work In Progress-------------------------------------%
 
+#--------------------------R API-------------------------------------#
+#--------------------------Deep Learning-----------------------------#
+library(deepnet);library(darch);library(FCNN4R);
+library(ForecastCombinations);
+library(EbayesThresh);library(HMM);library(markovchain)
+#------------------------Data Processing-----------------------------#
+library(XML);library(xml2);library(XML2R);library(seqinr);
+library(methods);library(dplyr);
+library(Matrix);library(pracma);library(expm)
+#-----------------------Signal Processing----------------------------#
+library(audio);library(tuneR);library(dtw);library(wavelets);
+library(dtwclust);library(TSclust);library(TSdist)
+library(wavethresh);library(waveslim);library(wavemulcor)
+library(PearsonDS);library(xtable);library(psych);library(adwave);
+library(biwavelet)
+#----------------------Parallel Processing---------------------------#
+library(parallel);library(microbenchmark)
+
+#---------------------Data Files in Music XML------------------------#
+album.1<-c("a.xml","b.xml","c.xml","d.xml","e.xml","f.xml","g.xml","h.xml","i.xml","j.xml")
+
+#-------------------------------------------------#
+#---Individual Tracks of Music Compositions---#
+#-------------------------------------------------#
+music.track.1<-read_xml(album.1[1])
+music.track.2<-read_xml(album.1[2])
+music.track.3<-read_xml(album.1[3])
+music.track.4<-read_xml(album.1[4])
+music.track.5<-read_xml(album.1[5])
+music.track.6<-read_xml(album.1[6])
+music.track.7<-read_xml(album.1[7])
+music.track.8<-read_xml(album.1[8])
+music.track.9<-read_xml(album.1[9])
+music.track.10<-read_xml(album.1[10])
+
+#---------------------------------------------------------------------#
+#------------------------------Functions------------------------------#
+#---------------------------------------------------------------------#
+
+#---------------------------------------------------------------------#
+#------------------------------Models---------------------------------#
+#---------------------------------------------------------------------#
+
+#---------------------------------------------------------------------#
+#------------------------------Analysis-------------------------------#
+#---------------------------------------------------------------------#
+
+#---------------------------------------------------------------------#
+#------------------------------Tables---------------------------------#
+#---------------------------------------------------------------------#
+
+
+#---------------------------------------------------------------------#
+#------------------------------Figures--------------------------------#
+#---------------------------------------------------------------------#
+
