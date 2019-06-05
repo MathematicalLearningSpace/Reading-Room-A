f0dfeb59faf2990d50cf516a6cbcd5e94b3d03cf
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-06-05 12:53:45 -0700

    Update Classroom Lecture Model Series _Course 2_Topic 1_Lecture_10_11_12_R_4.R

--- Classroom Lecture Model Series _Course 2_Topic 1_Lecture_10_11_12_R_4.R
+++ Classroom Lecture Model Series _Course 2_Topic 1_Lecture_10_11_12_R_4.R
@@ -29,14 +29,94 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 
 #------------MAPK-----------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+MAPK.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+MAPK.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------Calcium--------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Calcium.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Calcium.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------cAMP-----------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+cAMP.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+cAMP.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------Chemokine------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Chemokine.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Chemokine.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------nf-Kappa-------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+nf.kappa.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+nf.kappa.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
