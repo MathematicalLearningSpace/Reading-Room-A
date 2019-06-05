1462c6506e975cd1ecaa814d64838aaa7b7863a9
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-06-05 12:48:33 -0700

    Update Classroom Lecture Model Series _Course 2_Topic 1_Lecture_16_17_18_R_6.R

--- Classroom Lecture Model Series _Course 2_Topic 1_Lecture_16_17_18_R_6.R
+++ Classroom Lecture Model Series _Course 2_Topic 1_Lecture_16_17_18_R_6.R
@@ -4,7 +4,7 @@
 
 #--------------------Work In Progress-------------------------------------#
 
-#------------------------------R API----------------------------------#
+#------------------------------R API-----------------------------------#
 library(deSolve);library(ReacTran);library(rootSolve);
 library(rcellminer);library(rcellminerData);library(bioCancer);
 library(trena);library(ctSGE);library(anamiR) 
@@ -29,18 +29,130 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 
 #------------Longevity-----------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Longevity.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Longevity.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------Immune System-------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Immune.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Immune.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------wNT-----------------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Wnt.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Wnt.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------Notch---------------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Notch.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Notch.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------HedgeHog------------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Hedgehog.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Hedgehog.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------TGF-Beta------------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+TGF.Beta.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+TGF.Beta.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------VEGF----------------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+VEGF.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+VEGF.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
