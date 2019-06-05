10a891b760ba35b15d05e00d73943a97950901ac
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-06-05 13:01:55 -0700

    Update Classroom Lecture Model Series _Course 2_Topic 1_Lecture_19_20_21_R_7.R

--- Classroom Lecture Model Series _Course 2_Topic 1_Lecture_19_20_21_R_7.R
+++ Classroom Lecture Model Series _Course 2_Topic 1_Lecture_19_20_21_R_7.R
@@ -29,16 +29,94 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 
 #------------Apelin-----------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Apelin.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Apelin.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = Apelin.system.equation.model.1, parms = Params.Equation) 
 
 #------------Hippo------------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Hippo.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Hippo.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = Hippo.system.equation.model.1, parms = Params.Equation) 
 
 #------------Jak-STAT---------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Jak.STAT.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Jak.STAT.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = Jak.STAT.system.equation.model.1, parms = Params.Equation) 
 
 #------------Insulin----------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Insulin.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Insulin.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = Insulin.system.equation.model.1, parms = Params.Equation) 
 
 #------------WDR--------------------#
-
-
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+WDR.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+WDR.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = WDR.system.equation.model.1, parms = Params.Equation) 
 
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
