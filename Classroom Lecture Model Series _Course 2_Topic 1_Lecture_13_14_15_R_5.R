985696df913aeb629ee4c08b00d576abe88f39e1
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-06-05 12:57:02 -0700

    Update Classroom Lecture Model Series _Course 2_Topic 1_Lecture_13_14_15_R_5.R

--- Classroom Lecture Model Series _Course 2_Topic 1_Lecture_13_14_15_R_5.R
+++ Classroom Lecture Model Series _Course 2_Topic 1_Lecture_13_14_15_R_5.R
@@ -29,10 +29,58 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 
 #------------FOX-O-----------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Fox.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Fox.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------Cell Cycle------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+Cell.Cycle.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+Cell.Cycle.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #------------mTOR------------------#
+#------------Parameter Models----------#
+sequence.time <- seq(0, 10^2, by = 0.1)
+Params.Initial<-c(X<-1)
+Params.Equation<-c(a11<-1)
+#------------Equation Systems----------#
+mTOR.system.equation.model.1<-function(sequence.time,Params.Initial, Params.Equation)
+{
+  with(as.list(c(Params.Equation, Params.Initial)), 
+       {
+	d.1.X.dt.1 = a11*X
+res <- c(d.1.X.dt.1)
+ list(res)
+       })
+}
+#-------Solutions----------------------#
+mTOR.system.equation.model.1.solution<- ode(y = Params.Initial, times = sequence.time, func = system.equation.model.1, parms = Params.Equation) 
 
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
