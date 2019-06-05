ea594dd23190f0e5a71e34d9c7261e2f004edda5
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-06-05 08:03:37 -0700

    Update Classroom Lecture Model Series _Course 2_Topic 1_Lecture_1.R

--- Classroom Lecture Model Series _Course 2_Topic 1_Lecture_1.R
+++ Classroom Lecture Model Series _Course 2_Topic 1_Lecture_1.R
@@ -22,6 +22,16 @@ library(sgnesR);library(rcdk);library(sqldf)
 #----------------------------------Data----------------------------------------------#
 #------------------------------------------------------------------------------------#
 
+Oncogenes.Sample <- c("ABL1", "ALK", "BRAF", "CCND1", "CCND3", "CCNE1", "CCNE2", 
+                       "CDC25A", "EGFR", "ERBB2", "EZH2", "FOS", "FOXL2", "HRAS", 
+                       "IDH1", "IDH2", "JAK2", "KIT", "KRAS", "MET", "MOS", "MYC", 
+                       "NRAS", "PDGFB", "PDGFRA", "PIK3CA", "PIK3CB", "PIK3CD", 
+                       "PIK3CG", "PIM1", "PML", "RAF1", "REL", "RET", "SRC", "STK11", 
+                       "TP63", "WNT10B", "WNT4", "WNT2B", "WNT9A", "WNT3", "WNT5A", 
+                       "WNT5B", "WNT10A", "WNT11", "WNT2", "WNT1", "WNT7B", "WISP1", 
+                       "WNT8B", "WNT7A", "WNT16", "WISP2", "WISP3", "FZD5", "FZD1")
+
+
 KEGG.organisms<-keggList("organism")
 KEGG.Brite<-keggList("brite")
 KEGG.organisms.df<-as.data.frame(KEGG.organisms)
