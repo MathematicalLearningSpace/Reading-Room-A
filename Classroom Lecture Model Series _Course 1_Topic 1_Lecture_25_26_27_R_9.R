af9ca9d9dc5ece1c0f2b450161da235bb805a21f
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-06-06 23:59:47 -0700

    Update Classroom Lecture Model Series _Course 1_Topic 1_Lecture_25_26_27_R_9.R

--- Classroom Lecture Model Series _Course 1_Topic 1_Lecture_25_26_27_R_9.R
+++ Classroom Lecture Model Series _Course 1_Topic 1_Lecture_25_26_27_R_9.R
@@ -31,11 +31,37 @@ library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
 #---------------------------------------------------------------------#
 #------------------------------Models---------------------------------#
 #---------------------------------------------------------------------#
+Model.1<-function (Time.Sequence, variables.intitial, Params.1)
+  {
+    with(as.list(c(Params.1, variables.intitial)), 
+         {
+           #------------Equations Designed in the Classroom-----#
+           d.1.x.dt.1<-a0
+           d.1.y.dt.1<-a1
+           d.1.z.dt.1<-a2
+           d.1.w.dt.1<-a3
+   res <- c(d.1.x.dt.1,
+           d.1.y.dt.1,
+           d.1.z.dt.1,
+           d.1.w.dt.1)
+  list(res)
+         })
+}                                                
+#----------------Parameters-----------#   
+initial.vector<-c(x=1,y=0,z=0,w=0)
+Params.1<-c(a0<-1,a1<-1,a2<-1,a3<-1,a4<-1,a5<-1)                                                    
+Time.Sequence<- seq(0, 48, by = 48/120) 
 
+#---------------------------------------------------------------------#
+#------------------------------Solutions------------------------------#
+#---------------------------------------------------------------------#
+model.system.solution.1 <- ode(y = initial.vector, times = Time.Sequence, 
+func = Model.1, parms = Parms.1)
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+options(digits = 3)
+model.system.1.summary<-summary(model.system.solution.1)
 #-------------------Topology------------------------------------------#
 
 #-------------------Equilibrium---------------------------------------#
@@ -57,9 +83,17 @@ library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
 #---------------------------------------------------------------------#
 
 #---------Figure 1---------------------#
-
+png(file = stringr::str_c("Figures//Example_",1,"_Figure_",0,".png"))
+Figure.1<-matplot(model.system.solution.1[,2:5], type = "l", lty = 1, lwd = c(2, 1, 1,1),
+                  col = c("darkred", "darkblue", "darkgreen","red"),
+                  xlab = "Time [min]", ylab= "Y",
+                  main = "Solution Example")
+grid()
+dev.off() 
 #---------Figure 2---------------------#
-
+png(file = stringr::str_c("Figures//Example_",1,"_Figure_",1,".png"))
+Figure.1<-scatterplotMatrix(model.system.solution.1[,2:5])
+dev.off() 
 #---------Figure 3---------------------#
 
 #---------Figure 4---------------------#
