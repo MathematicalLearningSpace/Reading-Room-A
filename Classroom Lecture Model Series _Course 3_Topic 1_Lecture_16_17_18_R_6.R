795e579105ea2fd6c33ae9567f6e35e21afecd3f
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 11:24:05 -0700

    Update Classroom Lecture Model Series _Course 3_Topic 1_Lecture_16_17_18_R_6.R

--- Classroom Lecture Model Series _Course 3_Topic 1_Lecture_16_17_18_R_6.R
+++ Classroom Lecture Model Series _Course 3_Topic 1_Lecture_16_17_18_R_6.R
@@ -21,7 +21,7 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 #------------------------------Data-----------------------------------#
 #---------------------------------------------------------------------#
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
 #------------Initial Value-------------------#
 #------------Parameter Model-----------------#
 
@@ -34,16 +34,83 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 
 #---------Signal Model-------------#
+Signal.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Signal.Model.1<-Signal.Model.1("1")
+test.Signal.Model.1
+
 #---------Spline Model-------------#
+Spline.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Spline.Model.1<-Spline.Model.1("1")
+test.Spline.Model.1
 
 #---Nonlinear Tridiagonal mRNA translation Ribosome Model-------------#
 
 #--------------------Solutions--------------------#
-
+Solution.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+   return(output)
+ }
+test.Solution.Model.1<-Solution.Model.1("1")
+test.Solution.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 
+
+#----------------------------------Optimization-----------------------#
+Optimization.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Optimization.Model.1<-Optimization.Model.1("1")
+test.Optimization.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
@@ -52,7 +119,14 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #----------Table 2-------------#
 #----------Table 3-------------#
 #----------Table 4-------------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
@@ -61,6 +135,20 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---Figure 2 for Classroom Presentation------#
 #---Figure 3 for Classroom Presentation------#
 #---Figure 4 for Classroom Presentation------#
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
+
 
 #-----------------------------References------------------------------#
 
