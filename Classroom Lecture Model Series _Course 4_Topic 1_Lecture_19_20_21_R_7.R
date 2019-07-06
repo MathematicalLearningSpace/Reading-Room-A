32e52bf9fe74eb41b51eb683a3bea017fd275d4b
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 10:37:50 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_19_20_21_R_7.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_19_20_21_R_7.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_19_20_21_R_7.R
@@ -16,7 +16,9 @@ library(Matrix);library(pracma);library(expm)
 #---------------------------------------------------------------------#
 #------------------------------Data-----------------------------------#
 #---------------------------------------------------------------------#
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
+Table.1<-data.frame();Table.2<-data.frame();Table.3<-data.frame();Table.4<-data.frame();
+Table.5<-data.frame();Table.6<-data.frame();Table.7<-data.frame();Table.8<-data.frame();
 #---------------------------------------------------------------------#
 #------------------------------Functions------------------------------#
 #---------------------------------------------------------------------#
@@ -24,16 +26,71 @@ library(Matrix);library(pracma);library(expm)
 #---------------------------------------------------------------------#
 #------------------------------Models---------------------------------#
 #---------------------------------------------------------------------#
-
+Composition.Model.Classical.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  output$Table.4<-Table.4.df
+  output$Table.5<-Table.5.df
+  output$Table.6<-Table.6.df
+  return(output)
+ }
+test.Composition.Model.Classical.1<-Composition.Model.Classical.1("1")
+test.Composition.Model.Classical.1
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
+#----------Table 1------#
+Table.5.TeX<-xtable::xtable(Table.5.df)
+#----------Table 2------#
+Table.6.TeX<-xtable::xtable(Table.6.df)
+#----------Table 3------#
+Table.7.TeX<-xtable::xtable(Table.7.df)
+#----------Table 4------#
+Table.8.TeX<-xtable::xtable(Table.8.df)
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
 
