f931b9e475d51f62caeaec3dce2bdc70515116cb
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 11:29:04 -0700

    Update Classroom Lecture Model Series _Course 3_Topic 1_Lecture_19_20_21_R_7.R

--- Classroom Lecture Model Series _Course 3_Topic 1_Lecture_19_20_21_R_7.R
+++ Classroom Lecture Model Series _Course 3_Topic 1_Lecture_19_20_21_R_7.R
@@ -37,15 +37,63 @@ Chaperone_xml <- fetch_pubmed_data(get_pubmed_ids("Chaperone"))
 #---------------------------------------------------------------------#
 
 #------------Chaperonin Model I----------------------#
-
+Chaperonin.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Chaperonin.Model.1<-Chaperonin.Model.1("1")
+test.Chaperonin.Model.1
 #------------Chaperonin Model II----------------------#
-
+Chaperonin.Model.2<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Chaperonin.Model.2<-Chaperonin.Model.2("1")
+test.Chaperonin.Model.2
 #------------Chaperonin Model III----------------------#
-
+Chaperonin.Model.3<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Chaperonin.Model.3<-Chaperonin.Model.3("1")
+test.Chaperonin.Model.3
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
@@ -57,7 +105,14 @@ Chaperone_xml <- fetch_pubmed_data(get_pubmed_ids("Chaperone"))
 #----------Table 3 Group C---------#
 
 #----------Table 4 Group D---------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
@@ -66,4 +121,16 @@ Chaperone_xml <- fetch_pubmed_data(get_pubmed_ids("Chaperone"))
 #-----------Figure 2-------------------#
 #-----------Figure 3-------------------#
 #-----------Figure 4-------------------#
-
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
