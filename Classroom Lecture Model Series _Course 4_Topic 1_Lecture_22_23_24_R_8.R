31fe15163e574c9bdd89e9a564532f977ba1aa0a
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 10:44:15 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_22_23_24_R_8.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_22_23_24_R_8.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_22_23_24_R_8.R
@@ -70,17 +70,77 @@ Acoustic.Index.Prototype<-function(X)
 #---------------------------------------------------------------------#
 
 #-----------Markov Models I--------------#
-
+Composition.Model.Markov.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Markov.1<-Composition.Model.Markov.1("1")
+test.Composition.Model.Markov.1
 #-----------Semi-Markov Models I---------#
-
+Composition.Model.Markov.Semi.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Markov.Semi.1<-Composition.Model.Markov.Semi.1("1")
+test.Composition.Model.Markov.Semi.1
 #-----------Music Machine Learning-------#
-
+Composition.Model.Machine.Learning.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Machine.Learning.1<-Composition.Model.Machine.Learning.1("1")
+test.Composition.Model.Machine.Learning.1
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 #------------------Music Collection Statistics---------#
-
+Music.Collection.Statistics.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Music.Collection.Statistics.1<-Music.Collection.Statistics.1(Music.Collection.1.1)
+test.Music.Collection.Statistics.1
 
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
@@ -91,11 +151,31 @@ Acoustic.Index.Prototype<-function(X)
 #--------------Tables 3-----------#
 #--------------Tables 4-----------#
 
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
-
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
 #-------------Figures 1------------#
 #-------------Figures 2------------#
 #-------------Figures 3------------#
