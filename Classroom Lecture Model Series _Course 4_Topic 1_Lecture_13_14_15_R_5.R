c585575bc071e90a4a7a6062f597e0d25b4d1dce
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 10:21:25 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_13_14_15_R_5.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_13_14_15_R_5.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_13_14_15_R_5.R
@@ -24,7 +24,7 @@ library(parallel);library(microbenchmark)
 
 #---------------------Data Files in Music XML------------------------#
 album.1<-c("a.xml","b.xml","c.xml","d.xml","e.xml","f.xml","g.xml","h.xml","i.xml","j.xml")
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
 #-------------------------------------------------#
 #---Individual Tracks of Music Compositions---#
 #-------------------------------------------------#
@@ -42,23 +42,72 @@ music.track.10<-read_xml(album.1[10])
 #---------------------------------------------------------------------#
 #------------------------------Functions------------------------------#
 #---------------------------------------------------------------------#
-
+  Z.dwt.1 <- wavelets::dwt(as.numeric(Z), filter = "la8")
+  Z.dwt.2 <- wavelets::dwt(as.numeric(Z), filter = "d4")
+  Z.dwt.3 <- wavelets::dwt(as.numeric(Z), filter = "haar")
+  Z.dwt.4 <- wavelets::dwt(as.numeric(Z), filter = "c6")
+  Z.dwt.list <- list( Z.dwt.1,  Z.dwt.2,  Z.dwt.3,  Z.dwt.4)
+  mra.out <- wavelets::mra(as.numeric(Z), n.levels=3, boundary="reflection")
 #---------------------------------------------------------------------#
 #------------------------------Models---------------------------------#
 #---------------------------------------------------------------------#
 
+Composition.Model.Classical.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Classical.1<-Composition.Model.Classical.1("1")
+test.Composition.Model.Classical.1
+
+Composition.Model.Jazz.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Jazz.1<-Composition.Model.Jazz.1("1")
+test.Composition.Model.Jazz.1
+
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
 
-#--------------Table 1----------------------------#
-#--------------Table 2----------------------------#
-#--------------Table 3----------------------------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
@@ -67,6 +116,18 @@ music.track.10<-read_xml(album.1[10])
 #--------------Figure 1----------------------------#
 #--------------Figure 2----------------------------#
 #--------------Figure 3----------------------------#
-
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
 
 
