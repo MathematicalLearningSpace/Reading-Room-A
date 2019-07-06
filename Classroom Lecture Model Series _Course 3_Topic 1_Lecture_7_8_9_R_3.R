e5dda7beaf08fa2928a4b775ddf96acae81fe31f
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 11:07:52 -0700

    Update Classroom Lecture Model Series _Course 3_Topic 1_Lecture_7_8_9_R_3.R

--- Classroom Lecture Model Series _Course 3_Topic 1_Lecture_7_8_9_R_3.R
+++ Classroom Lecture Model Series _Course 3_Topic 1_Lecture_7_8_9_R_3.R
@@ -21,7 +21,7 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 #------------------------------Data-----------------------------------#
 #---------------------------------------------------------------------#
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
 #---------------------------------------------------------------------#
 #------------------------------Functions------------------------------#
 #---------------------------------------------------------------------#
@@ -31,14 +31,78 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 
 #--------Normal Mode Analysis-----------------#
-
+NMA.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.NMA.Model.1<-NMA.Model.1("1")
+test.NMA.Model.1
 #--------Compound Discovery Design------------#
-
+CCD.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.CCD.Model.1<-CCD.Model.1("1")
+test.CCD.Model.1
 #--------QSAR Models--------------------------#
-
+QSAR.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.QSAR.Model.1<-QSAR.Model.1("1")
+test.QSAR.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
+
+#----------------------------------Optimization--------------------------------------#
+Optimization.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Optimization.Model.1<-Optimization.Model.1("1")
+test.Optimization.Model.1
 
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
@@ -48,7 +112,14 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #-----------Table 2-------------------#
 #-----------Table 3-------------------#
 #-----------Table 4-------------------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
@@ -57,3 +128,16 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #------------Figure 1---------------------#
 #------------Figure 2---------------------#
 #------------Figure 3---------------------#
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
