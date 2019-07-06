63b2e6a63974bd3be7194a022bf56c54de0a24f8
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 10:52:22 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_25_26_27_R_9.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_25_26_27_R_9.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_25_26_27_R_9.R
@@ -20,7 +20,7 @@ library(recommenderlab);library(readr);library(GA)
 #---------------------------------------------------------------------#
 #------------------------------Data-----------------------------------#
 #---------------------------------------------------------------------#
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
 #---------------------------------------------------------------------#
 #------------------------------Functions------------------------------#
 #---------------------------------------------------------------------#
@@ -32,25 +32,77 @@ library(recommenderlab);library(readr);library(GA)
 #------------Item-based Collaborative Filtering Recommender Model-----#
 
 #------------Composition Models I------------------#
-
+Composition.Model.Jazz.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Jazz.1<-Composition.Model.Jazz.1("1")
+test.Composition.Model.Jazz.1
 #------------Composition Models II-----------------#
-
+Composition.Model.Jazz.2<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Jazz.2<-Composition.Model.Jazz.1("1")
+test.Composition.Model.Jazz.2
 #------------Composition Models III----------------#
 
-
+Composition.Model.Jazz.3<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Composition.Model.Jazz.3<-Composition.Model.Jazz.1("1")
+test.Composition.Model.Jazz.3
 
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
 
 #----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
 #----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
 #----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
 #----------Table 4------#
+Table.4.TeX<-xtable::xtable(T
 
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
@@ -60,4 +112,16 @@ library(recommenderlab);library(readr);library(GA)
 #----------Figure 2------#
 #----------Figure 3------#
 #----------Figure 4------#
-
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
