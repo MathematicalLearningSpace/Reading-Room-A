340e9b29ba103637edff3f61fdd8c102eedab4cd
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 07:16:59 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_1.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_1.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_1.R
@@ -13,6 +13,30 @@ library(jsonlite);library(rjson);library(xtable)
 
 #----------------------------------User Defined Modules and Functions----------------#
 
+
+#-----------------------------------Music Model--------------------------------------#
+Music_Model<-function(W,X,Y,Z,Visualization=TRUE)
+{
+setwd("Music Model/Music Sequences/XML Data")
+Music.Composition.1<-"";Music.Composition.2<-"";Music.Composition.3<-"";Music.Composition.4<-"";Music.Composition.5<-"";
+Music.Composition.6<-"";Music.Composition.7<-"";Music.Composition.8<-"";Music.Composition.9<-"";Music.Composition.10<-"";
+Notes.By.Measure<-list();Notes.Duration<-list();Notes.By.Step<-list();
+Notes.By.Octave<-list();Notes.By.Pitch<-list();Model.1<-list(); Model.Chain.1<-list();notes.series<-list();
+ Music.files<-list.files()
+ Music.Composition.1<-read_xml(Music.files[1])
+ Music.Composition.2<-read_xml(Music.files[2])
+ Music.Composition.3<-read_xml(Music.files[3])
+ Music.Composition.4<-read_xml(Music.files[4])
+ Music.Composition.5<-read_xml(Music.files[5])
+ Music.Composition.6<-read_xml(Music.files[6])
+ Music.Composition.7<-read_xml(Music.files[7])
+ Music.Composition.8<-read_xml(Music.files[8])
+ Music.Composition.9<-read_xml(Music.files[9])
+ Music.Composition.10<-read_xml(Music.files[10])
+#-----------------------Presentation and Organization--------------
+  output<-list()
+  return(output)
+}
 #----------------------------------Network Designs-----------------------------------#
 
 #----------------------------------Equation Systems----------------------------------#
