4a3bde38b2b467016fd035a9d9c4eab5282982a4
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 10:09:48 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_4_5_6_R_2.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_4_5_6_R_2.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_4_5_6_R_2.R
@@ -1,4 +1,7 @@
 #-------------------------------------------------------------------------#
+library(XML);library(xml2);library(XML2R);library(seqinr);
+library(methods);library(dplyr);
+library(Matrix);library(pracma);library(expm)
 #--------------------Classroom Lecture Model Series-----------------------#
 #-------------------------------------------------------------------------#
 
@@ -6,24 +9,68 @@
 #----------------------------------R Source Files------------------------------------#
 
 #----------------------------------Data----------------------------------------------#
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
 #----------------------------------Transformations-----------------------------------#
 
 #----------------------------------User Defined Modules and Functions----------------#
 
 #----------------------------------Network Designs-----------------------------------#
-
+Network.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Network.Model.1<-Network.Model.1("1")
+test.Network.Model.1
 #----------------------------------Equation Systems----------------------------------#
 
 #----------------------------------Parameter Tables----------------------------------#
 
 #----------------------------------Network Analysis----------------------------------#
-
+Network.Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Network.Analysis.Model.1<-Network.Analysis.Model.1("1")
+test.Network.Analysis.Model.1
 #----------------------------------Optimization--------------------------------------#
 
 #----------------------------------Natural Language Description----------------------#
 
 #----------------------------------Tables--------------------------------------------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 #----------------------------------Figures-------------------------------------------#
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
 #----------------------------------Discussion----------------------------------------#
