05dae7f4b237e74a4d0ea72060cfe50fd6f4f172
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 18:33:53 -0700

    Update Classroom Lecture Model Series _Course 1_Topic 1_Lecture_22_23_24_R_8.R

--- Classroom Lecture Model Series _Course 1_Topic 1_Lecture_22_23_24_R_8.R
+++ Classroom Lecture Model Series _Course 1_Topic 1_Lecture_22_23_24_R_8.R
@@ -17,7 +17,7 @@ library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
 #---------------------------------------------------------------------#
 #------------------------------Data-----------------------------------#
 #---------------------------------------------------------------------#
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
 #---------------------------------------------------------------------#
 #-----------------------Review Notes----------------------------------#
 #---------------------------------------------------------------------#
@@ -26,7 +26,19 @@ library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
 #---------------------------------------------------------------------#
 #------------------------------Functions------------------------------#
 #---------------------------------------------------------------------#
-
+Transformation.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Transformation.1<-Transformation.1("1")
+test.Transformation.1
 #---------------------------------------------------------------------#
 #------------------------------Models---------------------------------#
 #---------------------------------------------------------------------#
@@ -34,7 +46,33 @@ library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
+#----------------------------------Optimization--------------------------------------#
+Optimization.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Optimization.Model.1<-Optimization.Model.1("1")
+test.Optimization.Model.1
 #-------------------Topology------------------------------------------#
 
 #-------------------Equilibrium---------------------------------------#
@@ -49,7 +87,23 @@ library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
 #---------Table 2-----------------------#
 #---------Table 3-----------------------#
 #---------Table 4-----------------------#
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 
+#----------Table 5------#
+Table.5.TeX<-xtable::xtable(Table.5.df)
+#----------Table 6------#
+Table.6.TeX<-xtable::xtable(Table.6.df)
+#----------Table 7------#
+Table.7.TeX<-xtable::xtable(Table.7.df)
+#----------Table 8------#
+Table.8.TeX<-xtable::xtable(Table.8.df)
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
