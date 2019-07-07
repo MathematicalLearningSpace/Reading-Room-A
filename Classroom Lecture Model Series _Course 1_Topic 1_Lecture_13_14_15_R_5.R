88ae12357dd7d6cc23a34d8fe957c3322c0ed6d7
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-07 16:25:31 -0700

    Update Classroom Lecture Model Series _Course 1_Topic 1_Lecture_13_14_15_R_5.R

--- Classroom Lecture Model Series _Course 1_Topic 1_Lecture_13_14_15_R_5.R
+++ Classroom Lecture Model Series _Course 1_Topic 1_Lecture_13_14_15_R_5.R
@@ -21,6 +21,20 @@ library(qgraph);library(qlcMatrix);library(rcrossref);library(PearsonDS);library
 #---------------------------------------------------------------------#
 #-----------------------Review Notes----------------------------------#
 #---------------------------------------------------------------------#
+Review.Notes<-function(X)
+ {
+ Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ 
+ }
+test.Review.Notes.1<-Review.Notes.1("1")
+test.Review.Notes.1
 
 
 #---------------------------------------------------------------------#
@@ -67,10 +81,52 @@ test.Model.Markov.Chain.1
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
+Analysis.1<-function(X)
+ {
+ Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ 
+ }
+test.Analysis.1<-Analysis.1("1")
+test.Analysis.1
 
 #---------Multinomial Confidence Intervals----------------------------#
+Confindence.Intervals.Multinomial.1<-function(X)
+ {
+ Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ 
+ }
+test.Confindence.Intervals.Multinomial.1<-Confindence.Intervals.Multinomial.1("1")
+test.Confindence.Intervals.Multinomial.1
 
 #---------Path Probabilities------------------------------------------#
+Path.Probabilities.1<-function(X)
+ {
+ Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ 
+ }
+test.Path.Probabilties.1<-Path.Probabilities.1("1")
+test.Path.Probabilities.1
 
 #------------Graph Theory:Distance------------------------------------#
 Graph.Theory.Distance.1<-function(X)
@@ -125,7 +181,14 @@ test.Graph.Theory.Spectra.1
 #------------Table 2---------------#
 #------------Table 3---------------#
 #------------Table 4---------------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 
 #---------------------------------------------------------------------#
 #----------Figures for Classroom Presentation-------------------------#
@@ -136,6 +199,19 @@ test.Graph.Theory.Spectra.1
 #-----------Figure 3---------------#
 #-----------Figure 4---------------#
 #-----------Figure 5---------------#
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',3,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
 
 
 
