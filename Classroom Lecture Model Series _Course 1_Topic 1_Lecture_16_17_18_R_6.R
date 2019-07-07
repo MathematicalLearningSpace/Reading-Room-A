6a1332020d3ce2cfd33cc672570d5cab1bb02154
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 18:30:35 -0700

    Update Classroom Lecture Model Series _Course 1_Topic 1_Lecture_16_17_18_R_6.R

--- Classroom Lecture Model Series _Course 1_Topic 1_Lecture_16_17_18_R_6.R
+++ Classroom Lecture Model Series _Course 1_Topic 1_Lecture_16_17_18_R_6.R
@@ -24,29 +24,120 @@ library(NMOF);library(xtable)
 #---------------------------------------------------------------------#
 
 #--------------------------Objective Functions------------------------#
-
+OF.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.OF.Model.1<-OF.Model.1("1")
+test.OF.Model.1
 #--------------------------Fitness Metrics----------------------------#
-
+FM.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.FM.Model.1<-FM.Model.1("1")
+test.FM.Model.1
 #--------------------------Loss Functions-----------------------------#
-
+Loss.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Loss.Model.1<-Loss.Model.1("1")
+test.Loss.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Models---------------------------------#
 #---------------------------------------------------------------------#
 
 #-----------Genetic Algorithm Optimization----------------------------#
-
+Optimization.GA.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Optimization.GA.Model.1<-Optimization.GA.Model.1("1")
+test.Optimization.GA.Model.1
 #-----------Differential Evolution Optimization------------------------#
-
+Optimization.DE.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Optimization.DE.Model.1<-Optimization.DE.Model.1("1")
+test.Optimization.DE.Model.1
 #-----------Particle Swarm Optimization-------------------------------#
-
+Optimization.PS.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Optimization.PS.Model.1<-Optimization.PS.Model.1("1")
+test.Optimization.PS.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
-
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
