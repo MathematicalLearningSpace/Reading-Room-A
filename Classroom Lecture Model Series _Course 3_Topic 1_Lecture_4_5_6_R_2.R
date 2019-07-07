dd26f30371ca910e83f0f284e31299ccf6915634
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-07 16:58:02 -0700

    Update Classroom Lecture Model Series _Course 3_Topic 1_Lecture_4_5_6_R_2.R

--- Classroom Lecture Model Series _Course 3_Topic 1_Lecture_4_5_6_R_2.R
+++ Classroom Lecture Model Series _Course 3_Topic 1_Lecture_4_5_6_R_2.R
@@ -32,15 +32,63 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #---------------------------------------------------------------------#
 
 #---------Translational and Transcriptional Error Models I------------#
-
+Error.Translation.Transcriptional.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Error.Translation.Transcriptional.Model.1<-Error.Translation.Transcriptional.Model.1("1")
+test.Error.Translation.Transcriptional.Model.1
 #---------Translational and Transcriptional Error Models II-----------#
-
+Error.Translation.Transcriptional.Model.2<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Error.Translation.Transcriptional.Model.2<-Error.Translation.Transcriptional.Model.2("1")
+test.Error.Translation.Transcriptional.Model.2
 #---------Translational and Transcriptional Error Models III----------#
-
+Error.Translation.Transcriptional.Model.3<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Error.Translation.Transcriptional.Model.3<-Error.Translation.Transcriptional.Model.3("1")
+test.Error.Translation.Transcriptional.Model.3
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
@@ -50,6 +98,15 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #-----------Table 3-------------------#
 #-----------Table 4-------------------#
 
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
+
 
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
@@ -58,3 +115,17 @@ library(corrplot);library(plot3D);library(scatterplot3d);library(rgl)
 #------------Figure 1---------------------#
 #------------Figure 2---------------------#
 #------------Figure 3---------------------#
+
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',3,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
