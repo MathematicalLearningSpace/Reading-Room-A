380c19a9873063f12e52c079458fe442737a8ae4
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-06 18:20:36 -0700

    Update Classroom Lecture Model Series _Course 1_Topic 1_Lecture_10_11_12_R_4.R

--- Classroom Lecture Model Series _Course 1_Topic 1_Lecture_10_11_12_R_4.R
+++ Classroom Lecture Model Series _Course 1_Topic 1_Lecture_10_11_12_R_4.R
@@ -15,7 +15,7 @@ library(dtwclust);library(dtw);library(TSMining)
 #---------------------------------------------------------------------#
 #------------------------------Data-----------------------------------#
 #---------------------------------------------------------------------#
-
+W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
 #---------------------------------------------------------------------#
 #-----------------------Review Notes----------------------------------#
 #---------------------------------------------------------------------#
@@ -71,27 +71,97 @@ test.Classifier.Cluster.Hierarchical..1
 #---------------------------------------------------------------------#
 
 #----------------------Nonlinear Tests--------------------------------#
-
-#---------------------Staionarity-------------------------------------#
-
+Tests.Nonlinear.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Tests.Nonlinear.1<-Tests.Nonlinear.1("1")
+test.Tests.Nonlinear.1
+#---------------------Stationarity-------------------------------------#
+Tests.Stationarity.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Tests.Stationarity.1<-Tests.Stationarity.1("1")
+test.Tests.Stationarity.1
 #---------------------Prediction--------------------------------------#
-
+Prediction.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Prediction.Model.1<-Prediction.Model.1("1")
+test.Prediction.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Analysis-------------------------------#
 #---------------------------------------------------------------------#
-
+Analysis.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Analysis.Model.1<-Analysis.Model.1("1")
+test.Analysis.Model.1
+#----------------------------------Optimization--------------------------------------#
+Optimization.Model.1<-function(X)
+ {
+  Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ }
+test.Optimization.Model.1<-Optimization.Model.1("1")
+test.Optimization.Model.1
 #---------------------------------------------------------------------#
 #------------------------------Tables---------------------------------#
 #---------------------------------------------------------------------#
+#----------Table 1------#
+Table.1.TeX<-xtable::xtable(Table.1.df)
+#----------Table 2------#
+Table.2.TeX<-xtable::xtable(Table.2.df)
+#----------Table 3------#
+Table.3.TeX<-xtable::xtable(Table.3.df)
+#----------Table 4------#
+Table.4.TeX<-xtable::xtable(Table.4.df)
 
-#----------Table 1-------------#
-#----------Table 2-------------#
-#----------Table 3-------------#
-#----------Table 4-------------#
-#----------Table 5-------------#
-#----------Table 6-------------#
-
-
+#----------Table 5------#
+Table.5.TeX<-xtable::xtable(Table.5.df)
+#----------Table 6------#
+Table.6.TeX<-xtable::xtable(Table.6.df)
+#----------Table 7------#
+Table.7.TeX<-xtable::xtable(Table.7.df)
+#----------Table 8------#
+Table.8.TeX<-xtable::xtable(Table.8.df)
 
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
