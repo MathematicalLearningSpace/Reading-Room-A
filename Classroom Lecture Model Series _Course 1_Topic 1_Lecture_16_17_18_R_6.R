aa947cacff5cf64286458677959e3e487af1f871
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-07 16:27:40 -0700

    Update Classroom Lecture Model Series _Course 1_Topic 1_Lecture_16_17_18_R_6.R

--- Classroom Lecture Model Series _Course 1_Topic 1_Lecture_16_17_18_R_6.R
+++ Classroom Lecture Model Series _Course 1_Topic 1_Lecture_16_17_18_R_6.R
@@ -17,7 +17,20 @@ library(NMOF);library(xtable)
 #---------------------------------------------------------------------#
 #-----------------------Review Notes----------------------------------#
 #---------------------------------------------------------------------#
-
+Review.Notes<-function(X)
+ {
+ Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ 
+ }
+test.Review.Notes.1<-Review.Notes.1("1")
+test.Review.Notes.1
 
 #---------------------------------------------------------------------#
 #------------------------------Functions------------------------------#
@@ -141,6 +154,18 @@ Table.4.TeX<-xtable::xtable(Table.4.df)
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
-
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',3,'.png'))
+op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
+hist(W, main="Title 1",xlab="X Value")
+text(4, 9, expression(hat(theta) == (W^t))
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(X, main="Title 2",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Y, main="Title 3",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+hist(Z, main="Title 4",xlab="Note Value")
+legend("topright", legend = paste(seq(1:7),LETTERS[1:7]),lty = 1, cex = .8, y.intersp = 1)
+par(op)
+dev.off()
 #---------------References--------------------------------------------#
 
