59be79a3730af1ea5b7d9ab42a78756233cb2624
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-08 13:48:00 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_4_5_6_R_2.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_4_5_6_R_2.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_4_5_6_R_2.R
@@ -1,7 +1,9 @@
 #-------------------------------------------------------------------------#
 library(XML);library(xml2);library(XML2R);library(seqinr);
 library(methods);library(dplyr);
-library(Matrix);library(pracma);library(expm)
+library(Matrix);library(pracma);library(expm);
+library(dplyr);library(audio);library(seewave)
+library(sound);library(soundecology);library(soundgen)
 #--------------------Classroom Lecture Model Series-----------------------#
 #-------------------------------------------------------------------------#
 
