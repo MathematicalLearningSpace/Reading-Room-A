9adb2cb8d0b475bee7e8a69bda08445a35484809
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-08 09:48:30 -0700

    Update Classroom Lecture Model Series _Course 4_Topic 1_Lecture_10_11_12_R_4.R

--- Classroom Lecture Model Series _Course 4_Topic 1_Lecture_10_11_12_R_4.R
+++ Classroom Lecture Model Series _Course 4_Topic 1_Lecture_10_11_12_R_4.R
@@ -17,14 +17,28 @@ library(Matrix);library(pracma);library(expm)
 library(audio);library(tuneR);library(dtw);library(wavelets);
 library(dtwclust);library(TSclust);library(TSdist)
 library(wavethresh);library(waveslim);library(wavemulcor)
-library(PearsonDS);library(xtable);library(psych);library(adwave);
-library(biwavelet)
+library(PearsonDS);library(xtable);library(psych);library(adwave);library(biwavelet)
+library(wmtsa);library(wavelets);library(wavemulcor)
+library(PearsonDS);library(xtable);library(psych);library(adwave);library(BootWPTOS)
+library(liftLRD);library(mwaved);library(mvLSW);library(unbalhaar)
 #----------------------Parallel Processing---------------------------#
 library(parallel);library(microbenchmark)
 
 #---------------------Data Files in Music XML------------------------#
 album.1<-c("A.xml","B.xml","C.xml","D.xml","E.xml","F.xml","G.xml","H.xml","I.xml","J.xml")
 W<-data.frame();X<-data.frame();Y<-data.frame();Z<-data.frame();
+pearson.N<-512
+pIIIpars <- list(shape=1, location=1, scale=1)
+error.pearson.3<-rpearsonIII(pearson.N,params=pIIIpars)
+experimental.data.1<-window(error.pearson.3, end=256)
+
+cusps.1 <- function(x) -0.5*abs(x-1)^0.5 -0.5* abs(x-2)^0.5 + 0.5*x + 3.5
+cusps.2 <- function(x) -1*abs(x-1)^0.75 -0.5* abs(x-8)^0.75 + 1*x + 3     
+
+x <- seq(0, 10, length=512)
+y.1 <- splus2R::signalSeries(cusps.1(x), x)
+y.2 <- splus2R::signalSeries(cusps.2(x), x)
+y.3<- makeHeaviSine(pearson.N)
 #-------------------------------------------------#
 #---Individual Tracks of Music Compositions---#
 #-------------------------------------------------#
@@ -104,12 +118,60 @@ Table.4.TeX<-xtable::xtable(Table.4.df)
 #---------------------------------------------------------------------#
 #------------------------------Figures--------------------------------#
 #---------------------------------------------------------------------#
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+Figure.1<-plot(W.tree.1$`3`$extrema, type="l", lty=1, col="black",xlab="Observations",ylab="Extrema")
+lines(W.tree.2$`3`$extrema,lty=2, col="green")
+lines(W.tree.3$`3`$extrema,lty=3, col="blue")
+lines(W.tree.4$`3`$extrema,lty=4, col="red")
+rug(W.tree.1$`3`$extrema, side=4, col="black")
+rug(W.tree.2$`3`$extrema, side=4, col="green")
+rug(W.tree.3$`3`$extrema, side=4, col="blue")
+rug(W.tree.4$`3`$extrema, side=4, col="red")
+legend("topright",c("Branch 5_1=Gaussian 1","Branch 5_2=Gaussian 2","Branch 5_3=Haar","Branch 5_4=Morlet"),
+       inset = .01,col=c("black","green","blue","red"),lwd=2,cex=0.8)
+dev.off()
 
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',2,'.png'))
+op <- par(mfrow = c(3, 3),mar=c(1,1,1,1))
+Figure.2<-plot(W.tree.1, xlab="(a)")
+plot(W.tree.2, xlab="(b)")
+plot(W.tree.3, xlab="(c)")
+plot(W.tree.4, xlab="(d)")
+par(op)
+dev.off()
+
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',3,'.png'))
+Figure.2<-densityBy(as.data.frame(as.data.frame(y.1)),xlab="CUSP Expression 1", ylab="Simulated",col="green")
+dev.off()
+
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',4,'.png'))
+Figure.3<-plot(holder.1$exponent, type="l",ylim=c(-4,1),lty=1,xlab="Observations", ylab="Exponent",col="black")
+lines(holder.2$exponent,lty=2, col="green")
+lines(holder.3$exponent,lty=3, col="blue")
+lines(holder.4$exponent,lty=4, col="red")
+rug( holder.1$exponent, side=4, col="black")
+rug( holder.2$exponent, side=4, col="green")
+rug( holder.3$exponent, side=4, col="blue")
+rug( holder.4$exponent, side=4, col="red")
+legend("bottomright",c("Holder_1=Gaussian 1","Holder_2=Gaussian 2","Holder_3=Haar","Holder_4=Morlet"),inset = .01,col=c("black","green","blue","red"),lwd=2,cex=0.8)
+dev.off()
+
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',5,'.png'))
+Figure.4<-plot(experimental.data.1, type="l",lty=1,ylim<-c(-1,5),xlab="Observations", ylab="value",col="black")
+lines(y.1,lty=2, col="green")
+lines(y.2,lty=3, col="blue")
+lines(y.3,lty=4,col="red")
+rug(experimental.data.1, side=4, col="black")
+rug(y.1, side=4, col="green")
+rug(y.2, side=4, col="blue")
+rug(y.3, side=4, col="red")
+legend("topleft",c("Pearson III","CUSP 1","CUSP 2","HeaviSine"),inset = .01,col=c("black","green","blue","red"),lwd=2,cex=0.8)
+dev.off()
 #----------Figure 1-----------------#
 #----------Figure 2-----------------#
 #----------Figure 3-----------------#
 #----------Figure 4-----------------#
-png(file = stringr::str_c('Figures//Example_',1,'_Figure_',1,'.png'))
+png(file = stringr::str_c('Figures//Example_',1,'_Figure_',6,'.png'))
 op <- par(mfrow = c(2,2),mar=c(3,3,3,3))
 hist(W, main="Title 1",xlab="X Value")
 text(4, 9, expression(hat(theta) == (W^t))
