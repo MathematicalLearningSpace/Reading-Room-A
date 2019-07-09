298d6928190f11366b2d7beb01791f1deadebd4f
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-07-09 04:27:48 -0700

    Update Classroom Lecture Model Series _Course 3_Topic 1_Lecture_16_17_18_R_6.R

--- Classroom Lecture Model Series _Course 3_Topic 1_Lecture_16_17_18_R_6.R
+++ Classroom Lecture Model Series _Course 3_Topic 1_Lecture_16_17_18_R_6.R
@@ -64,6 +64,23 @@ Ribosome.Model.1<-function(X)
 test.Ribosome.Model.1<-Ribosome.Model.1("1")
 test.Ribosome.Model.1
 
+Ribosome.Model.2<-function(X)
+ {
+ Table.1.df<-data.frame(); Table.2.df<-data.frame(); Table.3.df<-data.frame();
+  
+ 
+ 
+  output<-list()
+  output$X<-X
+  output$Table.1<-Table.1.df
+  output$Table.2<-Table.2.df
+  output$Table.3<-Table.3.df
+  return(output)
+ 
+ }
+test.Ribosome.Model.2<-Ribosome.Model.2("1")
+test.Ribosome.Model.2
+
 #---------Signal Model-------------#
 Signal.Model.1<-function(X)
  {
