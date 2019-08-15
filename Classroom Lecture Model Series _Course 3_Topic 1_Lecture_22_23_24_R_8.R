0489ee958b39d8c33d8ca87e94aee7073d1ebc04
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-08-15 10:03:39 -0700

    Update Classroom Lecture Model Series _Course 3_Topic 1_Lecture_22_23_24_R_8.R

--- Classroom Lecture Model Series _Course 3_Topic 1_Lecture_22_23_24_R_8.R
+++ Classroom Lecture Model Series _Course 3_Topic 1_Lecture_22_23_24_R_8.R
@@ -70,37 +70,57 @@ Proteasome.Model.1<-function(X)
   #----------------------------------------------------------------------------------------------------------------------------#
   for(i in 1:k){protein.model.list[[i]]<-read.pdb(protein.model.files.pdb[i])}
   bio.Unit.1 <- biounit(protein.model.list[[3]]);names(bio.Unit.1)	
-  #---------------------------------------#
+   #---------------------------------------#
   #-------Chain Sequence Modeling---------#
   #---------------------------------------#
+  
+  Sequence.Chain.Model.1<-function(X){output<-list();return(output)}
   
   #-----------------------------------------------------#
   #-----------------------Residue Selection-------------#
   #-----------------------------------------------------#
+  
+  Operator.Selector.Residue<-function(X){ output<-list(); return(output)}
  
   #---------------------------------------#
   #-------Network Dependency Modeling-----#
   #---------------------------------------#	
 
+  Graph.Dependent.Model<-function(X){ output<-list(); return(output)}
+  
   #---------------------------------------#
   #-------Deformation energies------------# 
   #---------------------------------------#	
-
+  Energy.Defomration.Model<-function(X){ output<-list(); return(output)}
+  
   #---------------------------------------#
   #----------Fluctuations-----------------# 
   #---------------------------------------#
-	
+  
+  Energy.Fluctuations.Model<-function(X){ output<-list(); return(output)}
+
   #---------------------------------------#
   #-------Torsion Analysis By Chain-------#
   #---------------------------------------#
+  
+  Chain.Torsion.Analysis.Model<-function(X){ output<-list(); return(output)}
 
   #---------------------------------------#
   #---cross-correlations------------------#
   #---------------------------------------#
   
+  Moment.1<-function(X){output<-list(); return(output)}
+  Moment.2<-function(X){output<-list(); return(output)}
+  Moment.3<-function(X){output<-list(); return(output)}
+  Moment.4<-function(X){output<-list(); return(output)}
+
   #---------------------------------------------#
   #---------------Graph Theory Analysis---------#
-#---------------------------------------------#
+  #---------------------------------------------#
+  
+Graph.Analysis.1<-function(X){output<-list(); return(output)}
+  
+	
   output<-list()
   output$X<-X
   output$Table.1<-Table.1.df
